1deb75e11ebb3eea79f93cc80c6c003d8b2e6249
Roberto <roberto.cuyar44@gmail.com>
2021-04-20 14:53:30 -0500

    Testing out new threads alongside join and interrupt methods.

---
+++ src/com/threads/AnotherThread.java
@@ -0,0 +1,18 @@
+package com.threads;
+
+import static com.threads.ThreadColor.ANSI_BLUE;
+
+public class AnotherThread extends Thread {
+
+    @Override
+    public void run(){
+        System.out.println(ANSI_BLUE + "Hello from " + currentThread().getName());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            System.out.println(ANSI_BLUE + "Another thread woke me up.");
+            return;
+        }
+        System.out.println(ANSI_BLUE + "Three seconds have passed and I'm awake.");
+    }
+}
